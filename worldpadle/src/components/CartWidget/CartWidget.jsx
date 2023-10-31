aa83e3a4dd1992a3e682548a1181a1d0fb29e97f
Gianfranco Taliano <gian.taliano@gmail.com>
2023-10-30 22:52:46 -0300

    Primera Entrega React

---
+++ worldpadle/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,9 @@
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+export const CartWidget = () => {
+  return (
+    <div>
+      2<AiOutlineShoppingCart style={{ fontSize: '2rem' }} />
+    </div>
+  );
+}
